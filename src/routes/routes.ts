74c47500cec977a16d6d761ff0bbc3ef47ba5a4a
kushagra03614812724 <kushagra100chhabra@gmail.com>
2026-01-18 15:00:48 +0530

    profile

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import multer from "multer";
 
-import {create_users , verify_otp, resend_otp, user_login ,auth_me} from "../controller/user_controller";
+import {create_users , verify_otp, resend_otp, user_login ,auth_me , user_profile_update ,get_user_by_id} from "../controller/user_controller";
 
 const router = express.Router();
 
@@ -13,5 +13,11 @@ router.post("/verify_otp", upload.none(), verify_otp);
 router.post("/resend-otp", upload.none(), resend_otp);
 router.post("/user_login", upload.none(), user_login);
 router.get("/auth_me", auth_me);
+router.post(
+  "/user_profile_update/:id",
+  upload.single("profile_picture"),
+  user_profile_update
+);
+router.get("/get_user_by_id/:id", get_user_by_id);
 
 export default router;
